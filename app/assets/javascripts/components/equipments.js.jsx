ff47a440fd66f80ddb49acef6bf18d99286d41ce
Sarav Shah <saravshah@yahoo.com>
2017-03-22 21:14:52 -0700

    Fixed so checkbox work when clicked

--- app/assets/javascripts/components/equipments.js.jsx
+++ app/assets/javascripts/components/equipments.js.jsx
@@ -1,47 +1,58 @@
 class Equipments extends React.Component {
-	constructor(props) {
-		super(props);
-		// equipments = props.data;
-		this.state = { isChecked: false };
-		this.onChange = this.onChange.bind(this); 
+constructor(props) {
+    super(props);
+    // equipments = props.data;
+    // var tempArray = fillCheckedArray(equipment);
+    arrayLength = props.data.length-1;
+    this.state = { isChecked: new Array(arrayLength).fill(false) };
 
-	}
+    console.log(this)
+    this.onChange = this.onChange.bind(this); 
 
-	onChange(){
-		this.setState({isChecked: !this.state.isChecked});
-	}
-	
-
-	render(){
-		var label = this.state.isChecked ? console.log("TRUE") : console.log("FALSE");
-		return(
-			<div>
-				<h1> Equipments  </h1>
-					<ul> {this.props.data.map(function(equipment){
-
-						
-	            		return <span key={equipment.id}>
-	            			<form>
-								<label>
-		            				<input
-		            				name="isChecked"
-		            				type="checkbox"
-		            				checked={this.state.isChecked}
-		            				onChange={this.onChange}
-		            				/>
-		            				<a href={'/equipment/' + equipment.id} > {equipment.name}</a>
-		            			</label>
-		            				{label}
-		            		</form>
-		            		</span>
-
-	            	    },this)
-						} 	
-					</ul>				
-			</div>
-  		)	
-  	}
 }
+
+
+//We now pass a parameter to access the relevant value in the array
+onChange(id){
+	console.log("id",id)
+    var tempArray = this.state.isChecked.slice();
+    console.log("temparraybefore!", tempArray)
+    tempArray[id] = !tempArray[id];
+    console.log("temparray",tempArray)
+    this.setState({isChecked: tempArray});
+}
+	
+render(){
+    return(
+        <div>
+            <h1> Equipments  </h1>
+                <ul> {this.props.data.map(function(equipment, index){
+
+
+                    return <span key={equipment.id}>
+                        <form>
+                            <label>
+                                <input
+                                name="isChecked"
+                                type="checkbox"
+                                checked={this.state.isChecked[equipment.id-1]}
+                                onChange={this.onChange.bind(this,index)}
+                                />
+                                <a href={'/equipment/' + equipment.id} > {equipment.name}</a>
+                                
+                            </label>
+                            <input type="submit" value="Submit" />
+                        </form>
+                        </span>
+
+                    },this)
+                    }   
+                </ul>               
+        </div>
+    )   
+}
+}
+
 
 
 
